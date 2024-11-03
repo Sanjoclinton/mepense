dc81e08233818918208d436857712080c8be821b
Sanjo Clinton <gclinton880@gmail.com>
2024-11-03 01:38:27 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,57 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        "id": "/",
+        "start_url": "/dashboard",
+        "scope": "/",
+        "name": "Mepense, the best expense app for your device",
+        "short_name": "Mepense",
+        "display": "standalone",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+  
+        "description": "The best expense app for tracking your day to day incomes and expenses",
+        "screenshots": [
+          {
+            "src": "screenshots/screenshot.png",
+            "sizes": "1277x994",
+            "type": "image/png",
+            "form_factor": "wide"
+          },
+          {
+            "src": "screenshots/screenshot.png",
+            "sizes": "1277x994",
+            "type": "image/png",
+            "form_factor": "narrow"
+          }
+        ],
+        "icons": [
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+          }
+        ]
+      }
+      
+      
+    }),
+  ],
+});
